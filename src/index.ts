1899f14a3d29323d85b638a34ee880cb68dfaaf2
Jacob Drury <jacobdrury75@gmail.com>
2021-07-08 23:50:09 -0500

    init

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { registerCommands, registerEvents } from './utils/registry';
+import config from '../slappey.json';
+import DiscordClient from './client/client';
+const client = new DiscordClient({});
+
+(async () => {
+    client.prefix = config.prefix || client.prefix;
+    await registerCommands(client, '../commands');
+    await registerEvents(client, '../events');
+    await client.login(config.token);
+})();
